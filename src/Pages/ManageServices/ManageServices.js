896658640e4b47b0ace2b9b5fdebc2dc78b418bd
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2021-10-30 23:27:50 +0600

    Delete Update

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -1,28 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+
 import "./ManageService.css";
 const ManageServices = () => {
-  const [services, setServices] = useState([]);
+ 
+  const [services, setServices] = useState([])
   useEffect(() => {
-    fetch("https://desolate-oasis-18622.herokuapp.com/services")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
-  const handleDelete = (id) => {
-    const url = `https://desolate-oasis-18622.herokuapp.com/services/${id}`;
-    fetch(url, {
-      method: "DELETE",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.deletedCount) {
-          alert("Successfully Delete This Package");
-          const remaining = services.filter(services._id !== id);
-          setServices(remaining);
-        }
-      });
-  };
+      fetch('https://desolate-oasis-18622.herokuapp.com/services')
+          .then(res => res.json())
+          .then(data => setServices(data))
+  }, [])
 
+  const handleDelete = id => {
+    const url = `https://desolate-oasis-18622.herokuapp.com/services/${id}`;
+      fetch(url, {
+          method: 'DELETE'
+
+      })
+          .then(res => res.json())
+          .then(data => {
+              console.log(data)
+              if (data.deletedCount) {
+                  const remaining = services?.filter(service => service._id !== id);
+                  setServices(remaining);
+                  alert('Remove From Database Successfully')
+
+              }
+
+          })
+
+  }
   return (
     <div>
       <h2 className=" my-5 services-header p-3 m-3 mx-auto">
